daee012a8e013697c19170873b4b3f0a8954a6af
‪Omar Yasser‬‏ <omary4576@gmail.com>
2026-01-23 02:30:05 +0200

    feat: create splash screen with animation

--- lib/core/app_assets/app_images.dart
+++ lib/core/app_assets/app_images.dart
@@ -3,18 +3,12 @@ abstract class AppImages {
 
   // Main Images
   static const String logo = '$_basePath/logo.png';
-  static const String onboarding1 = '$_basePath/onboarding_1.png';
-  static const String onboarding2 = '$_basePath/onboarding_2.png';
-  static const String onboarding3 = '$_basePath/onboarding_3.png';
+  static const String splashBackground = '$_basePath/splash_background.png';
   static const String whiteNile = '$_basePath/white_nile.png';
   static const String blueNile = '$_basePath/blue_nile.png';
-  static const String chooseRole = '$_basePath/choose_role.png';
-  static const String exploreApp = '$_basePath/explore_app.png';
-  static const String forgetPassword = '$_basePath/forget_password.png';
-  static const String signUpLogo = '$_basePath/sign_up_logo.png';
-  static const String success = '$_basePath/success.png';
-  static const String welcomeAvatar = '$_basePath/welcome_avatar.png';
+  // static const String onboarding1 = '$_basePath/onboarding_1.png';
+  // static const String onboarding2 = '$_basePath/onboarding_2.png';
+  // static const String onboarding3 = '$_basePath/onboarding_3.png';
 
   // Test
-  
 }
--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -1,10 +1,14 @@
 import 'package:go_router/go_router.dart';
 import 'package:guide_me/core/routes/app_routes.dart';
+import 'package:guide_me/features/splash/presentation/screens/splash_screen.dart';
 
 abstract class AppRouter {
   static final appRouter = GoRouter(
     routes: [
-      
+      GoRoute(
+        path: AppRoutes.splashScreen,
+        builder: (context, state) => const SplashScreen(),
+      ),
     ],
   );
 }
