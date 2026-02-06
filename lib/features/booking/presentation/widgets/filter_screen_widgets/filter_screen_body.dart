a0f9de5e0391e6ae0e01d2296a0f3ad2a35323c3
Mena <menar5693@gmail.com>
2026-02-06 23:37:33 +0200

    feat: replace CustomCheckBox with CategoryCheckBox and update imports

--- lib/features/booking/presentation/widgets/filter_screen_widgets/filter_screen_body.dart
+++ lib/features/booking/presentation/widgets/filter_screen_widgets/filter_screen_body.dart
@@ -6,7 +6,7 @@ import 'package:guide_me/core/routes/app_routes.dart';
 import 'package:guide_me/core/styles/app_colors.dart';
 import 'package:guide_me/core/styles/app_text_styles.dart';
 import 'package:guide_me/core/widgets/app_button.dart';
-import 'package:guide_me/features/booking/presentation/widgets/filter_screen_widgets/custom_check_box.dart';
+import 'package:guide_me/features/booking/presentation/widgets/filter_screen_widgets/Category_check_box.dart';
 import 'package:guide_me/features/booking/presentation/widgets/filter_screen_widgets/price_slider.dart';
 import 'package:guide_me/features/booking/presentation/widgets/filter_screen_widgets/people_slider.dart';
 
@@ -64,16 +64,23 @@ class FilterScreenBody extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 30.p),
             child: Text(
               context.l10n.category,
-              style: const TextStyle(
+              style: TextStyle(
                 fontFamily: 'Montserrat',
                 fontWeight: FontWeight.w600,
                 color: AppColors.natural6,
-                fontSize: 16,
+                fontSize: 16.fs,
               ),
             ),
           ),
-
-          const CustomCheckBox(),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.p,
+            ),
+            child: const CategoryCheckBox(),
+          ),
 
           SizedBox(
             height: size.height * 0.020,
@@ -95,11 +102,11 @@ class FilterScreenBody extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 30.p),
             child: Text(
               context.l10n.numberofperson,
-              style: const TextStyle(
+              style: TextStyle(
                 fontFamily: 'Montserrat',
                 fontWeight: FontWeight.w600,
                 color: AppColors.natural6,
-                fontSize: 16,
+                fontSize: 16.fs,
               ),
             ),
           ),
