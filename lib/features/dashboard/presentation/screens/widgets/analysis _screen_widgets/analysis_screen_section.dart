5e1f41aa4ea0fc637e684fb95546fa8f836f703f
Mena <menar5693@gmail.com>
2026-02-26 22:42:32 +0200

    feat: add feedback and recent widgets to analysis screen section

--- lib/features/dashboard/presentation/screens/widgets/analysis _screen_widgets/analysis_screen_section.dart
+++ lib/features/dashboard/presentation/screens/widgets/analysis _screen_widgets/analysis_screen_section.dart
@@ -4,6 +4,9 @@ import 'package:guide_me/core/extentions/context_extentions.dart';
 import 'package:guide_me/core/responsive/reponsive_extention.dart';
 import 'package:guide_me/core/styles/app_colors.dart';
 import 'package:guide_me/core/styles/app_text_styles.dart';
+import 'package:guide_me/features/booking/presentation/widgets/guide_profile_widgets/comment_item_list_view.dart';
+import 'package:guide_me/features/booking/presentation/widgets/guide_profile_widgets/feed_back_travel.dart';
+import 'package:guide_me/features/booking/presentation/widgets/guide_profile_widgets/recent_widget.dart';
 import 'package:guide_me/features/dashboard/presentation/screens/widgets/analysis%20_screen_widgets/analysis_state_card_section.dart';
 
 class AnalysisScreenSection extends StatelessWidget {
@@ -60,10 +63,21 @@ class AnalysisScreenSection extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(
+            height: 13,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.p),
+            child: const FeedBackTravel(),
+          ),
+          const SizedBox(
+            height: 35,
+          ),
+          const RecentWidget(),
+
+          const CommentItemListView(),
         ],
       ),
     );
   }
 }
-
-
