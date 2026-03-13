65996ba24ab00e4eb590f86971a5ff3d6ca9c0a5
Mena <menar5693@gmail.com>
2026-03-14 01:35:54 +0200

    feat: implement AuthRepoImple with login, register, resend password, reset password, send forget password, and verify forget password methods

---
+++ lib/features/auth/data/repo/auth_repo_imple.dart
@@ -0,0 +1,94 @@
+import 'package:dart_either/src/dart_either.dart';
+import 'package:guide_me/core/errors/error_handler.dart';
+import 'package:guide_me/core/errors/failure.dart';
+import 'package:guide_me/features/auth/data/models/data_source/Auth_remote_data_source.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/resend_password_model.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/resend_password_request_model.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/reset_password_request_model.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/reset_password_response_model.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/send_forget_password_model.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/send_forget_password_request_model.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/verfiy_forget_password_model.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/verfiy_forget_password_request_model.dart';
+import 'package:guide_me/features/auth/data/models/login_model.dart';
+import 'package:guide_me/features/auth/data/models/login_request_model.dart';
+import 'package:guide_me/features/auth/data/models/register_mode.dart';
+import 'package:guide_me/features/auth/data/models/register_request_model.dart';
+import 'package:guide_me/features/auth/domain/repo/auth_repo.dart';
+
+class AuthRepoImple extends AuthRepo {
+  final AuthRemoteDataSource authRemoteDataSource;
+
+  AuthRepoImple(this.authRemoteDataSource);
+  @override
+  Future<Either<Failure, LoginresponseModel>> login(
+    LoginRequestModel request,
+  ) async {
+    try {
+      final result = await authRemoteDataSource.login(request);
+      return Right(result);
+    } catch (e) {
+      return Left(ErrorHandler.handle(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, RegisterResponseModel>> register(
+    RegisterRequestModel request,
+  ) async {
+    try {
+      final result = await authRemoteDataSource.register(request);
+      return Right(result);
+    } catch (e) {
+      return Left(ErrorHandler.handle(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, ResendPasswordResponseModel>> resendPassword(
+    ResendPasswordRequestModel request,
+  ) async {
+    try {
+      final result = await authRemoteDataSource.resendPassword(request);
+      return Right(result);
+    } catch (e) {
+      return Left(ErrorHandler.handle(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, ResetPasswordResponseModel>> resetPassword(
+    ResetPasswordRequestModel request,
+  ) async {
+    try {
+      final result = await authRemoteDataSource.resetPassword(request);
+      return Right(result);
+    } catch (e) {
+      return Left(ErrorHandler.handle(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, SendForgetPasswordResponsetModel>> sendForgetPassword(
+    SendForgetPasswordRequestModel request,
+  ) async {
+    try {
+      final result = await authRemoteDataSource.sendForgetPassword(request);
+      return Right(result);
+    } catch (e) {
+      return Left(ErrorHandler.handle(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, VerifyForgetPasswordResponse>> verifyForgetPassword(
+    VerifyForgetPasswordRequestModel request,
+  )async {
+    try {
+      final result = await authRemoteDataSource.verifyForgetPassword(request);
+      return Right(result);
+    }  catch (e) {
+      return Left(ErrorHandler.handle(e.toString()));
+    }
+  }
+}
