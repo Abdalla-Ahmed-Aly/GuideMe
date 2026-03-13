1b12ad5c0b731595831efa5d83b9b38b0e93f288
Mena <menar5693@gmail.com>
2026-03-13 18:53:28 +0200

    feat: restructure forget password models and update imports for clarity

--- lib/features/auth/domain/repo/auth_repo.dart
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -2,10 +2,10 @@ import 'package:dart_either/dart_either.dart';
 import 'package:guide_me/core/errors/failure.dart';
 import 'package:guide_me/features/auth/data/models/login_model.dart';
 import 'package:guide_me/features/auth/data/models/register_mode.dart';
-import 'package:guide_me/features/auth/data/models/resend_password_model.dart';
-import 'package:guide_me/features/auth/data/models/reset_password_response_model.dart';
-import 'package:guide_me/features/auth/data/models/send_forget_password_model.dart';
-import 'package:guide_me/features/auth/data/models/verfiy_forget_password_model.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/resend_password_model.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/reset_password_response_model.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/send_forget_password_model.dart';
+import 'package:guide_me/features/auth/data/models/forget_password/verfiy_forget_password_model.dart';
 
 abstract class AuthRepo {
   Future<Either<Failure, LoginresponseModel>> lgoinrequest();
