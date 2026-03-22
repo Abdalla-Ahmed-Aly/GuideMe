84d4b0c0feaeeca51b5e9f5174122ba448c81027
Mena <menar5693@gmail.com>
2026-03-22 05:59:53 +0200

    add multie select in home featch

--- lib/features/home/presentation/widgets/select_interests_widgets/interests_grid_view.dart
+++ lib/features/home/presentation/widgets/select_interests_widgets/interests_grid_view.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:guide_me/core/extentions/context_extentions.dart';
 import 'package:guide_me/core/responsive/reponsive_extention.dart';
+import 'package:guide_me/core/routes/app_routes.dart';
+import 'package:guide_me/core/widgets/app_button.dart';
 import 'package:guide_me/core/widgets/show_elegant_snackbar.dart';
 import 'package:guide_me/features/home/presentation/cubits/get_catogry_copy/get_catogry_copy_cubit.dart';
 import 'package:guide_me/features/home/presentation/widgets/select_interests_widgets/intersests_card.dart';
 
-class InterestsGridView extends StatelessWidget {
+class InterestsGridView extends StatefulWidget {
   const InterestsGridView({
     super.key,
   });
 
+  @override
+  State<InterestsGridView> createState() => _InterestsGridViewState();
+}
+
+class _InterestsGridViewState extends State<InterestsGridView> {
+  List<String> selectedCategories = [];
+  @override
   @override
   Widget build(BuildContext context) {
     return BlocListener<GetCatogryCopyCubit, GetCatogryCopyState>(
@@ -29,28 +39,61 @@ class InterestsGridView extends StatelessWidget {
           } else if (state is GetCatogryCopySuccess) {
             final categories = state.categories;
 
-            return GridView.builder(
-              padding: EdgeInsets.symmetric(horizontal: 70.p),
-              physics: const ClampingScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 50,
-                mainAxisSpacing: 50,
-                childAspectRatio: context.isPortrait ? 2 / 2.5 : 2.5 / 2,
-              ),
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                final category = categories[index];
+            return Column(
+              children: [
+                Expanded(
+                
+                  child: GridView.builder(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 70.p,
+                      vertical: 20.p,
+                    ),
+                    physics: const ClampingScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 50,
+                      mainAxisSpacing: 50,
+                      childAspectRatio: context.isPortrait ? 2 / 2.5 : 2.5 / 2,
+                    ),
+                    itemCount: categories.length,
+                    itemBuilder: (context, index) {
+                      final category = categories[index];
+                      return IntersestsCard(
+                        isSelected: selectedCategories.contains(category.id),
+                        onTap: () {
+                          setState(() {
+                            if (selectedCategories.contains(category.id)) {
+                              selectedCategories.remove(category.id);
+                            } else {
+                              selectedCategories.add(category.id);
+                            }
+                          });
+                        },
+                        image: category.image,
+                        title: category.name,
+                      );
+                    },
+                  ),
+                ),
 
-                return IntersestsCard(
-                  image: category.image,
-                  title: category.name,
-                );
-              },
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20.p,
+                    vertical: 20.p,
+                  ),
+                  child: AppButton(
+                    onPressed: selectedCategories.isNotEmpty
+                        ? () {
+                            context.go(AppRoutes.touristNavigationBarScreen);
+                          }
+                        : null,
+                    text: 'Continue',
+                  ),
+                ),
+              ],
             );
           }
 
-          /// 👇 default (initial state)
           return const Center(child: CircularProgressIndicator());
         },
       ),
--- lib/features/home/presentation/widgets/select_interests_widgets/intersests_card.dart
+++ lib/features/home/presentation/widgets/select_interests_widgets/intersests_card.dart
@@ -1,40 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:guide_me/core/routes/app_routes.dart';
+
 import 'package:guide_me/core/styles/app_text_styles.dart';
 
 class IntersestsCard extends StatelessWidget {
-  const IntersestsCard({super.key, required this.title, required this.image});
+  const IntersestsCard({
+    super.key,
+    required this.title,
+    required this.image,
+    required this.isSelected,
+    required this.onTap,
+  });
+
   final String title;
   final String image;
+  final bool isSelected;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       splashColor: Colors.transparent,
-      onTap: () {
-        context.go(AppRoutes.touristNavigationBarScreen);
-      },
+      highlightColor: Colors.transparent,
+      onTap: onTap, 
       child: Column(
-        mainAxisSize: MainAxisSize.min,
         children: [
           Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.network(
-                image,
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
+            child: Stack( 
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    image,
+                    width: double.infinity,
+                    height: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                if (isSelected)
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.black.withValues(alpha:  0.5), 
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        Icons.check_circle,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                  ),
+              ],
             ),
           ),
           const SizedBox(height: 8),
           Text(
             title,
-            style: AppTextStyles.poppinsRegular12,
-            textAlign: TextAlign.center,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
+            style: AppTextStyles.poppinsRegular12.copyWith(
+              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+            ),
           ),
         ],
       ),
