92fac9d846c3dfb9e526e686672e57416858b23b
Mena <menar5693@gmail.com>
2026-04-05 01:25:45 +0200

    feat: enhance AnalysisCubit with loading state and error handling

--- lib/features/dashboard/presentation/manager/Analysis_Cubit/analysis_cubit.dart
+++ lib/features/dashboard/presentation/manager/Analysis_Cubit/analysis_cubit.dart
@@ -1,8 +1,24 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:guide_me/core/errors/failure.dart';
+import 'package:guide_me/features/dashboard/data/models/Analysis_model/analysis_response_model.dart';
+import 'package:guide_me/features/dashboard/domain/use_case/get_analysis_use_case%20.dart';
 
 part 'analysis_cubit_state.dart';
 
 class AnalysisCubitCubit extends Cubit<AnalysisCubitState> {
-  AnalysisCubitCubit() : super(AnalysisCubitInitial());
+  AnalysisCubitCubit(this.getAnalysisUseCase) : super(AnalysisCubitInitial());
+  final GetAnalysisUseCase getAnalysisUseCase;
+  void safeEmit(AnalysisCubitState state) {
+    if (!isClosed) emit(state);
+  }
+
+  Future<void> getAnalysis() async {
+    safeEmit(AnalysisCubitLoading());
+    final result = await getAnalysisUseCase.call();
+    result.fold((failure) {
+      safeEmit(AnalysisCubitFailure(failure));
+    }, (data) {
+      safeEmit(AnalysisCubitSuccess(data));
+    });
+  }
 }
