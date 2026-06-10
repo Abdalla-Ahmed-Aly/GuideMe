709d04b925064efbb16675713b98b3833692b9fa
Abdalla-Ahmed-Aly <160060789+Abdalla-Ahmed-Aly@users.noreply.github.com>
2026-06-10 18:30:41 +0300

    finish the app and deployment

--- lib/core/constants/maps_constants.dart
+++ lib/core/constants/maps_constants.dart
@@ -1,5 +1,5 @@
 abstract class MapsConstants {
   static const String mapUrlTemplate =
       'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-  static const String userAgentPackageName = 'com.example.guide_me';
+  static const String userAgentPackageName = 'com.guide_me.app';
 }
